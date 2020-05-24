c29ce39c960bf97741119bbcd7950fd4737eefef
Toast <toast.tsao@phoenixnet.com.tw>
2020-05-24 16:03:53 +0800

    Add DockerFile

--- SignalrServier/Startup.cs
+++ SignalrServier/Startup.cs
@@ -1,18 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SignalrServier.Hubs;
 using SignalrServier.Repository;
-using SignalrServier.Services;
 using SignalrServier.Services.Chat;
 using StackExchange.Redis;
 
